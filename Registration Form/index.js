3c338f3ab1366251fd573cb092bf2a90ed72ae1a
Mayank Satish Lakkewar <mayanklakkewar01@gmail.com>
2024-09-09 23:05:29 +0530

    Add files via upload

---
+++ Registration Form/index.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const dotenv =  require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3500;
+
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.24nhg.mongodb.net/?registrationFormDB`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+//registration schema
+const registrationSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const registration = mongoose.model("Registration", registrationSchema);
+
+app.use(bodyParser.urlencoded ({extended : true}));
+app.use(bodyParser.json());
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname + "/pages/index.html")
+})
+
+app.post("/register", async(req,res) => {
+    try{
+        const {name, email, password} = req.body;
+
+        const existingUser = await Registration.findOne({email : email})
+        //checking for exsisiting user 
+        if(!existingUser){
+            const registrationData = new Registration({
+                name,
+                email,
+                password
+           });
+           await registrationData.save();
+           res.redirect("/success");
+        }
+        else{
+            console.log("User already Exsist")
+            res.redirect(__dirname + "/pages/error.html")
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.redirect("error");
+    }
+})
+
+app.get("/success", (req, res) =>{
+    res.sendFile(__dirname + "/pages/success.html")
+})
+
+app.get("/error", (req,res) => {
+    res.sendFile(__dirname + "/pages/error.html")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}) 
